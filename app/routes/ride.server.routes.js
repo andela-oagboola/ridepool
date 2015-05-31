c57b47effd274452581d44bb1fd4f90c603424e5
Andela Dev <olaide.agboola@andela.co>
2015-05-31 19:47:25 +0100

    implememnted backend routes

--- app/routes/ride.server.routes.js
+++ app/routes/ride.server.routes.js
@@ -2,10 +2,10 @@
 var ride = require('./../controllers/rides.server.controller');
 
 module.exports = function (app) {
-  app.routes('/ride/add').get(ride.addRide);
+  app.route('/ride/add').post(ride.addRide);
   app.route('/ride/:rideId').get(ride.getUnigueRide);
   app.route('/ride/:rideId/update').put(ride.editRide);
   app.route('/ride/:rideId/delete').delete(ride.deleteRide);
-  app.route('/ride/:userId').get(ride.getRideByUser);
-  app.routes('/rides').get(ride.getRides);
+  app.route('/ride/user/:userId').get(ride.getRideByUser);
+  app.route('/rides').get(ride.getRides);
 };
