e977fce8fe06226c30565238f376d8ba7e4bfc10
Andela Dev <olaide.agboola@andela.co>
2015-05-31 18:55:29 +0100

    added controller for rides

---
+++ app/controllers/rides.server.controller.js
@@ -0,0 +1,61 @@
+'use strict';
+var Ride = require('./../models/rides.server.model');
+
+module.exports = {
+ //create ride
+ addRide: function (req, res) {
+  Ride.create(req.body, function (err, ride) {
+    if (err) {
+      res.json(err);
+    }
+    else {
+      res.json(ride);
+    }
+  });
+ },
+
+ getRides: function (req, res) {
+  Ride.find({}, function (err, rides) {
+    if (err) {
+      res.json(err);
+    }
+    else {
+      res.json(rides);
+    }
+  });
+ },
+
+ deleteRide: function(req, res) {
+  Ride.delete({'_id': req.params.rideId}, function(err, res) {
+    if (err) {
+      res.json(err);
+    }
+    else {
+      res.json(res);
+    }
+  });
+ },
+
+ getUnigueRide: function (req, res) {
+  Ride.find({'_id': req.params.rideId}, function (err, ride) {
+    if (err) {
+      res.json(err);
+    }
+    else {
+      res.json(ride);
+    }
+  });
+ },
+
+ editRide: function (req, res) {
+  Ride.findByIdAndUpdate(req.params.rideId, req.body, function (err, ride) {
+    if (err) {
+      res.json(err);
+    }
+    else {
+      res.json(ride);
+    }
+  });
+ }
+
+};
